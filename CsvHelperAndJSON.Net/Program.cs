1f23167d93fab20f4f80dafa275b273ba70edf75
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-26 15:36:00 +0530

    [Sachin] Adding welcome message

---
+++ CsvHelperAndJSON.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsvHelperAndJSON.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the csvhelper and json .net Program");
+        }
+    }
+}
